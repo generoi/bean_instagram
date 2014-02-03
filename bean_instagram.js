f030ef1112dac25f4c9ffafa11da13bf276f6949
Axel Wiklund <aunion@gmail.com>
2014-02-03 16:10:13 +0100

    Fixed js error

--- bean_instagram.js
+++ bean_instagram.js
@@ -2,7 +2,7 @@ Drupal.behaviors.bean_instagram = {
   attach: function (context, settings) {
     var instances = Drupal.settings.bean_instagram && Drupal.settings.bean_instagram.instances;
     if (instances) {
-      for (var i = 0; l = instances.length, i < l; i++) {
+      for (var i = 0, l = instances.length; i < l; i++) {
        new Instafeed(instances[i]).run();
      }
    }
