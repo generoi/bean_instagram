f97262cea262384e6aa3abf640e241296b5e41cb
Oskar Schöldström <public@oxy.fi>
2014-02-13 13:54:09 +0100

    fix code conventions

--- plugins/bean/InstagramBean.class.php
+++ plugins/bean/InstagramBean.class.php
@@ -28,9 +28,9 @@ class InstagramBean extends BeanPlugin {
     $form = array();
     $form['settings'] = array(
       '#type' => 'fieldset',
-      '#tree' => 1,
-      '#title' => t('Settings')
-      );
+      '#tree' => TRUE,
+      '#title' => t('Settings'),
+    );
 
     $form['settings']['get'] = array(
       '#type' => 'select',
@@ -40,55 +40,55 @@ class InstagramBean extends BeanPlugin {
         'tagged' => t('Tagged'),
         'location' => t('Location'),
         'user' => t('User'),
-        ),
+      ),
       '#required' => TRUE,
       '#default_value' => $bean->settings['get'],
-      '#description' => t('Customize what Instafeed fetches.')
-      );
+      '#description' => t('Customize what Instafeed fetches.'),
+    );
 
     $form['settings']['accessToken'] = array(
       '#type' => 'textfield',
       '#title' => t('Access Token'),
-      '#description' => t('A valid oAuth token. Required if you wish to search by user id.'),  
-      '#default_value' => $bean->settings['accessToken']
-      );
+      '#description' => t('A valid oAuth token. Required if you wish to search by user id.'),
+      '#default_value' => $bean->settings['accessToken'],
+    );
 
     $form['settings']['clientId'] = array(
       '#type' => 'textfield',
       '#title' => t('Client ID'),
-      '#description' => t('Your API client id from Instagram. Required.'),  
+      '#description' => t('Your API client id from Instagram. Required.'),
       '#default_value' => $bean->settings['clientId'],
-      '#required' => TRUE
-      );
+      '#required' => TRUE,
+    );
 
     $form['settings']['locationId'] = array(
       '#type' => 'textfield',
       '#title' => t('Location ID'),
-      '#description' => t('Unique id of a location to get.'),  
+      '#description' => t('Unique id of a location to get.'),
       '#default_value' => $bean->settings['locationId'],
       '#states' => array(
         'visible' => array(
           ':input[name$="settings[get]"]' => array('value' => 'location'),
-        )
-      )
+        ),
+      ),
     );
 
     $form['settings']['tagName'] = array(
       '#type' => 'textfield',
       '#title' => t('Tag Name'),
-      '#description' => t('Name of the tag to get.'),  
+      '#description' => t('Name of the tag to get.'),
       '#default_value' => $bean->settings['tagName'],
       '#states' => array(
         'visible' => array(
           ':input[name$="settings[get]"]' => array('value' => 'tagged'),
-        )
-      )
-      );
+        ),
+      ),
+    );
 
     $form['settings']['sortBy'] = array(
       '#type' => 'select',
       '#title' => t('Sort by'),
-      '#description' => t('Sort the images in a set order.'),  
+      '#description' => t('Sort the images in a set order.'),
       '#options' => array(
         'most-recent' => t('Most Recent'),
         'least-recent' => t('Least Recent'),
@@ -96,39 +96,39 @@ class InstagramBean extends BeanPlugin {
         'least-liked' => t('Least Liked'),
         'most-commented' => t('Most Commented'),
         'least-commented' => t('Least Commented'),
-        'random' => t('Random')
-        ),
+        'random' => t('Random'),
+      ),
       '#required' => TRUE,
-      '#default_value' => $bean->settings['sortBy']
-      );
+      '#default_value' => $bean->settings['sortBy'],
+    );
 
     $form['settings']['links'] = array(
       '#type' => 'checkbox',
       '#title' => t('Links'),
-      '#description' => t('Wrap the images with a link to the photo on Instagram.'), 
-      '#default_value' => $bean->settings['links']
-      );
+      '#description' => t('Wrap the images with a link to the photo on Instagram.'),
+      '#default_value' => $bean->settings['links'],
+    );
 
     $form['settings']['limit'] = array(
       '#type' => 'textfield',
       '#title' => t('Limit'),
-      '#description' => t('Maximum number of Images to add. Max of 60.'),  
+      '#description' => t('Maximum number of Images to add. Max of 60.'),
       '#default_value' => $bean->settings['limit'],
-      '#required' => TRUE
-      );
+      '#required' => TRUE,
+    );
 
     $form['settings']['resolution'] = array(
       '#type' => 'select',
       '#title' => t('Resolution'),
-      '#description' => t('Size of the images to get.'),  
+      '#description' => t('Size of the images to get.'),
       '#options' => array(
         'thumbnail' => t('Thumbnail'),
         'low_resolution' => t('Low Resolution'),
-        'standard_resolution' => t('Standard Resolution')
-        ),
+        'standard_resolution' => t('Standard Resolution'),
+      ),
       '#required' => TRUE,
-      '#default_value' => $bean->settings['resolution']
-      );
+      '#default_value' => $bean->settings['resolution'],
+    );
 
     return $form;
 
